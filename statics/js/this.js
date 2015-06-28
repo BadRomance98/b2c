e3da43309b493eb33910de2ad19e66fc987da58b
BadRomance <247788490@qq.com>
2015-06-28 18:05:40 +0800

    兼容性

--- statics/js/this.js
+++ statics/js/this.js
@@ -1,3 +1,8 @@
+
+
+       
+
+
 $(function(){
    $(".pt").bind("mouseover",function(event){
    		$(".pt").css("opacity","0.5");
@@ -13,13 +18,56 @@ $(function(){
 })
 
 
-$(document).ready(function(){
+$(function(){
     //限制字符个数
-    $(".beyond").each(function(){
-        var maxwidth=50;
+     var Sys = {};
+        var ua = navigator.userAgent.toLowerCase();
+        var s;
+        (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+        (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+        (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+        (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+
+
+
+       if(Sys.ie==undefined) {
+      
+      $(".beyond").each(function(){
+        var maxwidth=30;
         if($(this).text().length>maxwidth){
             $(this).text($(this).text().substring(0,maxwidth));
             $(this).html($(this).html()+'...');
         }
     });
+  }else{
+  
+   var wordLimit=function(){
+    $(".beyond").each(function(){
+        var copyThis = $(this.cloneNode(true)).hide().css({
+            'position': 'absolute',
+            'width': 'auto',
+            'overflow': 'visible'
+        }); 
+        $(this).after(copyThis);
+        if(copyThis.width()>$(this).width()){
+            $(this).text($(this).text().substring(0,$(this).html().length-4));
+            $(this).html($(this).html()+'...');
+            copyThis.remove();
+            wordLimit();
+        }else{
+            copyThis.remove(); //清除复制
+            return;
+        }
+    });
+}
+wordLimit();
+}
+  
 });
+
+
+
+
+
+  

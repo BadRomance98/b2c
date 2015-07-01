0e84c05bd7321d744e5187be43e76e3b671b7041
zxnui <zhuxunui@gmail.com>
2015-07-01 19:03:48 +0800

    print log to file

--- models/base.go
+++ models/base.go
@@ -41,26 +41,26 @@ func init() {
 	//同步数据结构
 	engine.Sync2(new(User), new(News), new(Media))
 
+	// 输出sql到log
+	var filelog string
+	filelog = "log/xorm-sql-" + strings.Split(time.Now().String(), " ")[0] + ".log"
+	f, err := os.OpenFile(filelog, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("日志文件建立失败:%s", err)
+		os.Exit(1)
+	}
+	engine.Logger = xorm.NewSimpleLogger(f)
+
 	//读取配置文件，设置xorm日志状态
 	showSQL, _ := conf.Cfg.GetValue("xorm", "showSql")
 	showDebug, _ := conf.Cfg.GetValue("xorm", "showDebug")
 	showError, _ := conf.Cfg.GetValue("xorm", "showError")
 	showWarn, _ := conf.Cfg.GetValue("xorm", "showWarn")
+
 	engine.ShowSQL = strings.EqualFold(showSQL, "true")
 	engine.ShowDebug = strings.EqualFold(showDebug, "true")
 	engine.ShowErr = strings.EqualFold(showError, "true")
 	engine.ShowWarn = strings.EqualFold(showWarn, "true")
-
-	//输出sql到log
-	var filelog string
-	filelog = "log/xorm-sql-" + strings.Split(time.Now().String(), " ")[0] + ".log"
-	f, err := os.Create(filelog)
-	if err != nil {
-		log.Fatalf("日志文件建立失败:%s", err)
-		os.Exit(1)
-	}
-	defer f.Close()
-	engine.Logger = xorm.NewSimpleLogger(f)
 
 	Xorm = engine
 }
